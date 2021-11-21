1ca8d1cb1a2000ab2deb80a93cf356ad2ee04a89
HarshKumar Jha <jhaharsh878@gmail.com>
2021-11-21 10:00:41 +0530

    added sukoku and modified sudoko_solver

--- sudoko_solver.c
+++ sudoko_solver.c
@@ -17,11 +17,15 @@ int IsSafe(int arr[N][N],int col,int row,int data)
          if(data==arr[i][col])    
             return 0;
     }
-    for (i = (3*(row/3));i < 3*((row/3)+1); i++)    //checking sqrt9xsqrt9 box inside 9x9 container. 
+
+    int startRow = row - row % 3,
+			startCol = col - col % 3;
+
+    for (i = 0;i < 3; i++)    //checking sqrt9xsqrt9 box inside 9x9 container. 
     {
-        for(j=3*(col/3);j < 3*((col/3)+1); j++)
+        for(j=0;j < 3; j++)
         {
-            if(data==arr[i][j]==1)    
+            if(data==arr[i+startRow][j+startCol])    
                return 0;
         }
     }
@@ -46,22 +50,22 @@ int Sudoko_solver(int arr[N][N],int col,int row)
 {
    // printf("a");
     
-      if(row>=N-1 && col==N)
+      if(row==N-1 && col==N)
       {
         //  printf("\nm\n");
           return 1 ;
       }
       
-      if(col>=N){
+      if(col==N){
       //printf("b");
-          row+=1;
+          row++;
           col=0;
       }
       
       
-      while(col<=N && arr[row][col]!=0)
+      while(col<N && arr[row][col]!=0)
       {
-         if(arr[row][col]!=0)
+         // if(arr[row][col]!=0)
              col+=1;
       }
       
@@ -70,10 +74,14 @@ int Sudoko_solver(int arr[N][N],int col,int row)
           row+=1;
           col=0;
       }
-       //printf("%d",col);
+      //  // printf("%d",col);
+
+        // if (arr[row][col] > 0)
+	       //     	return Sudoko_solver(arr, col+1, row);
+
        if(arr[row][col]==0){
 
-      for(int j=1;j<=N;){
+      for(int j=1;j<=N;j++){
       // printf("c");
          if(IsSafe(arr,col,row,j)==1)
          {
@@ -84,48 +92,48 @@ int Sudoko_solver(int arr[N][N],int col,int row)
                   print(arr);
               }*/
        
-             int note= Sudoko_solver(arr,col+1,row);//recursion
-
-             if(note==1){
+             /* int note= */ if(Sudoko_solver(arr,col+1,row)==1)//recursion
+                                        return 1;
+             // if(note==1){
                  //printf("L\n");
-                return 1;
-             }
+                
+             // }
                     //printf("%d",arr[i][row]);
-
-             arr[row][col]=0;  // backtracking
+                    
+                    arr[row][col]=0; // backtracking
          }
-         j=j+1;
+              
+         
+         // j=j+1;
     } 
-       }
-       else{
-          if( Sudoko_solver(arr, col+1, row)==1)
-          {
-              return 1;
-          }
-       }
+  }
+  else{
+     if( Sudoko_solver(arr, col+1, row)==1)
+     {
+         return 1;
+     }
+  }
           
-        //   printf("f\n");
-        //   print(arr);
-        //   printf("\n");
-    //      printf("k\n");
-     return 0;
-      
-     
+        //   printf("f\n"); 
+          print(arr);
+          printf("\n");
+         printf("k\n");
+     return 0;  
 }
 
 int main()
 {
    /* printf("N=");
     scanf("%d",&N);*/
-   int grid[N][N] = { {3, 0, 6, 5, 0, 8, 4, 0, 0}, 
-         {5, 2, 0, 0, 0, 0, 0, 0, 0}, 
-         {0, 8, 7, 0, 0, 0, 0, 3, 1}, 
-         {0, 0, 3, 0, 1, 0, 0, 8, 0}, 
-         {9, 0, 0, 8, 6, 3, 0, 0, 5}, 
-         {0, 5, 0, 0, 9, 0, 6, 0, 0}, 
-         {1, 3, 0, 0, 0, 0, 2, 5, 0}, 
-         {0, 0, 0, 0, 0, 0, 0, 7, 4}, 
-         {0, 0, 5, 2, 0, 6, 3, 0, 0} };
+   int grid[N][N] = { {1, 0, 0,  0, 0, 7,  0, 9, 0}, 
+                      {0, 3, 0,  0, 2, 0,  0, 0, 8}, 
+                      {0, 0, 9,  6, 0, 0,  5, 0, 0}, 
+                      {0, 0, 5,  3, 0, 0,  9, 0, 0}, 
+                      {0, 1, 0,  0, 8, 0,  0, 0, 2}, 
+                      {6, 0, 0,  0, 0, 4,  0, 0, 0}, 
+                      {3, 0, 0,  0, 0, 0,  0, 1, 0}, 
+                      {0, 4, 0,  0, 0, 0,  0, 0, 7}, 
+                      {0, 0, 7,  0, 0, 0,  3, 0, 0} };
    
     int sudoko=Sudoko_solver(grid,0,0);
    // printf("d\n");
@@ -134,15 +142,18 @@ int main()
         printf("\n.............................................printing output.................................................\n");
         print(grid);
     }
+    else{
+     printf("soln does not exist");
+    }
 
     return 1;
 }
-        //   3 1 6 5 7 8 4 9 2 
-        //   5 2 9 1 3 4 7 6 8 
-        //   4 8 7 6 2 9 5 3 1 
-        //   2 6 3 4 1 5 9 8 7 
-        //   9 7 4 8 6 3 1 2 5 
-        //   8 5 1 7 9 2 6 4 3 
-        //   1 3 8 9 4 7 2 5 6 
-        //   6 9 2 3 5 1 8 7 4 
-        //   7 4 5 2 8 6 3 1 9 
+// 1 6 2 8 5 7 4 9 3 
+// 5 3 4 1 2 9 6 7 8 
+// 7 8 9 6 4 3 5 2 1 
+// 4 7 5 3 1 2 9 8 6 
+// 9 1 3 5 8 6 7 4 2 
+// 6 2 8 7 9 4 1 3 5 
+// 3 5 6 4 7 8 2 1 9 
+// 2 4 1 9 3 5 8 6 7 
+// 8 9 7 2 6 1 3 5 4 
