46b040a57bdc36d3946714ae2f35d79c42868160
HarshKumar Jha <jhaharsh878@gmail.com>
2022-01-30 17:17:30 +0530

    modified product_array_puzzel.c to more optimized solution

--- Product_array_puzzle.c
+++ Product_array_puzzle.c
@@ -1,18 +1,37 @@
+// tiime compllexity of this solution is O(n) and is one of the optimised solution
+
 #include<stdio.h>
 
 int main(){
-   int arr[]={10,5,7,2,11};
+   int arr[]={1,0};
    int size=sizeof(arr)/sizeof(arr[0]);
    int product_array[size];
-
+   int temp2=1;
+  int count=0;
    int temp=1;
    for(int i=0;i<size;i++){
+             if(arr[i]==0){
+                count=count+1;
+            }
+        }
+
+   for(int i=0;i<size;i++){
       temp=temp*arr[i];
+       if(arr[i]!=0 && count==1){
+
+                temp2=temp2*arr[i];
+            }
    }
 
     for(int i=0;i<size;i++)
     {
-       product_array[i]=temp/arr[i];
+       if(arr[i]!=0)
+          product_array[i]=temp/arr[i];
+          else if(arr[i]==0 && count>1)
+          product_array[i]=temp;
+          else
+          product_array[i]=temp2;  // this is the case for chacking if arr[i]==0 and count==1;
+
     }
 
     for(int i=0;i<size;i++)
