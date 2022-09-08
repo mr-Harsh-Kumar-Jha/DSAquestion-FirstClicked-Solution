5c1402e464e454e68cadae89f358f3c42aa3af37
HarshKumar Jha <jhaharsh878@gmail.com>
2022-09-08 21:51:29 +0530

    added reverse_integer.cpp

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 long long int reversDigits(int num){
     long long int rev_num = 0;
-    while (num > 0) {
-        rev_num = (long long int)(rev_num * 10 + num % 10);
+    while (num != 0) {
+        rev_num = rev_num * 10 + num % 10;
         num = num / 10;
     }
     return rev_num;
