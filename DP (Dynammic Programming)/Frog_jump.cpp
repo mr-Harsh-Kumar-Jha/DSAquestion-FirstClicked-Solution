7bb0a3b4866cceb50d99629c14ac0aec8f34e26c
HarshKumar Jha <jhaharsh878@gmail.com>
2022-09-17 01:47:17 +0530

    added Frog_jump

---
+++ DP (Dynammic Programming)/Frog_jump.cpp
@@ -0,0 +1,21 @@
+// this is a top down approach of DP and here time complexity is exponential
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int minPow(int stairs , vector<int>& pow , vector<int>& dp){
+   if(stairs == 1) return 0;
+   if(dp[stairs]!=0) return dp[stairs];
+   int left = minPow(stairs-1 , pow, dp)+ abs(pow[stairs]-pow[stairs-1]);
+    int right = INT_MAX;
+   if(stairs>1)
+     right = minPow(stairs-2, pow, dp)+abs(pow[stairs]-pow[stairs-2]);
+   return dp[stairs]= min(left ,  right);
+}
+
+int main(){
+   vector<int>pow{10,30,20,60,40};
+   vector<int> dp(pow.size()+1,0);
+   minPow(pow.size(),pow,dp);
+   cout<<dp[pow.size()];
+}
