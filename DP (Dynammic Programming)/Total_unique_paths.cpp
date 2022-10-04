9ba8bc25191409daf31459f6a7500d61ac856202
HarshKumar Jha <jhaharsh878@gmail.com>
2022-10-04 12:12:32 +0530

    added Total_unique_paths

--- DP (Dynammic Programming)/Total_unique_paths.cpp
+++ DP (Dynammic Programming)/Total_unique_paths.cpp
@@ -1,3 +1,11 @@
+// you are present at point A which is the top-left cell of an M*N matrix , your destination is point 'B' , which is the bottom-right cell of the same matrix . your task is to find the total no of unique paths from point 'A' to the point 'B'  . In other words you will be given the dimension of the matrix as integers M and N , your task is to find the total no of unique paths from the cell Matix[i][j] to Matrix[m-1][n-1] .
+
+// to traverse in the matrix , you can either mmove right or down at each step . for eg. in a give point Matrix[i][j] you can either move to matrix[i+1][j] or matrix[i][j+1] .
+
+//-----------------------------------------------------------------------------
+
+//this is a top down approach and the time complexity of the solution is O(m*n) and space complexity is O(m*n)+O(n) .
+
 #include<bits/stdc++.h>
 using namespace std;
 
