cf7233d9d7e2ef45c118914a244b26ef9322353b
HarshKumar Jha <jhaharsh878@gmail.com>
2022-10-04 12:07:39 +0530

    added Total_unique_paths

--- DP (Dynammic Programming)/Total_unique_paths.cpp
+++ DP (Dynammic Programming)/Total_unique_paths.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int totalUniquePath(int n,int m,vector<vector<int>>&dp){
    if(n==0 && m==0) return 1;
    if(n<0 || m<0) return 0;
-   if(dp[m][n]!=-1) return dp[m][n];
-   int left = totalUniquePath(n-1,m,dp);
-   int up = totalUniquePath(n,m-1,dp);
-   return dp[m][n]=left+up;
+   if(dp[n][m]!=-1) return dp[n][m];
+   int left= totalUniquePath(n,m-1,dp);
+   int up = totalUniquePath(n-1,m,dp);
+   return dp[n][m]=left+up;
 }
 
 int main(){
    int n,m;
    cin>>n>>m;
-   vector<vector<int>>dp(m,vector<int>(n,-1));
-   cout<<totalUniquePath(n,m,dp)<<endl;
+   vector<vector<int>>dp(n,vector<int>(m,-1));
+   cout<<dp.size()<<endl;
+   cout<<totalUniquePath(n-1,m-1,dp)<<endl;
 }
