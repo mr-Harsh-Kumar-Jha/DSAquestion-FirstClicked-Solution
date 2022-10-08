1fce75ac91297f8b15b8c7f48ff0081bd6727054
HarshKumar Jha <jhaharsh878@gmail.com>
2022-10-09 01:26:33 +0530

    added Triangle_path_sum

--- Triangle_path_sum.cpp
+++ Triangle_path_sum.cpp
@@ -1,4 +1,12 @@
-// you are give a triangular array list 'Triangle' . Your task is to retun a minimum path sum to reach from the top to the bottom row .
+// You are given a triangular array/list 'TRIANGLE'. Your task is to return the minimum path sum to reach from the top to the bottom row.
+
+// The triangle array will have N rows and the i-th row, where 0 <= i < N will have i + 1 elements.
+
+// You can move only to the adjacent number of row below each step. For example, if you are at index j in row i, then you can move to i or i + 1 index in row j + 1 in each step.
+
+//-----------------------------------------------------------------------------------
+
+// this is a recursive approach and time complexity of this solution is exponential.
 
 #include <bits/stdc++.h>
 using namespace std;
