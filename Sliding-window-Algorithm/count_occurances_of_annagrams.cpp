d2d9fc09020cb70d2b2a6308049e0b86eb5d2a90
HarshKumar Jha <jhaharsh878@gmail.com>
2022-11-25 18:25:51 +0530

    added count_occurances_of_annagrams

---
+++ Sliding-window-Algorithm/count_occurances_of_annagrams.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string s = "abcbaxcbavahbdcab";
+   string subS = "abc";
+   int n = s.size();
+   int k = subS.size(), start = 0, end = 0;
+   int ans=0;
+   map<char, int> m;
+   for (int i = 0; i < k; i++){
+      m[subS[i]]++;
+   }
+   int count = m.size();
+   while (end < n){
+       if (m.find(s[end]) != m.end()){
+            m[s[end]]--;
+            if (m[s[end]] == 0) count--;
+      }
+      if ((end - start+1) <k) end++;
+      else if((end-start+1)==k){
+         if (count == 0) ans++;
+         if (m.find(s[start]) != m.end()){
+            m[s[start]]++;
+            if (m[s[start]] == 1) count++;
+         }
+         start++;
+         // if (m.find(s[end]) != m.end())
+         // {
+         //    m[s[end]]--;
+         //    if (m[s[end]] == 0)
+         //       count--;
+         //    // else if(m[s[end]]<0) count++;
+         // }
+         end++;
+      }
+   }
+   cout << ans<< endl;
+   // cout<<ans<<endl;
+}
