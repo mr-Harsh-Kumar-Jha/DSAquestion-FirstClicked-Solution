1c0fc8a8892c343c4563fb312c01232740be05bc
HarshKumar Jha <jhaharsh878@gmail.com>
2022-12-24 15:05:40 +0530

    modified Knapsack_M2

--- DP (Dynammic Programming)/Knapsack_M2.cpp
+++ DP (Dynammic Programming)/Knapsack_M2.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main(){
    vector<int> val{160, 350, 120};
-   vector<int> wt{1, 2, 1};
+   vector<int> wt{2, 3, 1};
    int w = 5;
    int n = val.size();
    vector<vector<int>> dp(n, vector<int>(w + 1, 0));
-   for (int i = 1; i <= w; i++) dp[0][i] = dp[0][i-1] + val[0];
+   for (int i = 1; i <= w; i++) if(i>=wt[0]) dp[0][i] = dp[0][i-wt[0]] + val[0];
    for (int i = 1; i < n; i++) {
       for (int j = 1; j <=w; j++){
          int noTake = 0+dp[i-1][j];
