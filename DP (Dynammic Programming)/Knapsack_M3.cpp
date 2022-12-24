06d37a6c6a70031685201654f947ac70e2e72dbb
HarshKumar Jha <jhaharsh878@gmail.com>
2022-12-24 15:30:13 +0530

    added Knapsack_M3

--- DP (Dynammic Programming)/Knapsack_M3.cpp
+++ DP (Dynammic Programming)/Knapsack_M3.cpp
@@ -1,6 +1,24 @@
-#include<bits/stdc++.h>
+//this is a space optimization technique 
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    
+   vector<int> val{160, 350, 120};
+   vector<int> wt{2, 3, 1};
+   int w = 5;
+   int n = val.size();
+   vector<int>prev(w+1,0);
+   for(int i=1;i<=w;i++) if(i>=wt[0]) prev[i]=prev[i-wt[0]]+val[0];
+   for(int i=1;i<n;i++){
+      vector<int>curr(w+1,0);
+       for(int j=1;j<=w;j++){
+         int noTake = 0+prev[j];
+         int take = INT_MIN;
+         if(j>=wt[i]) take=val[i]+curr[j-wt[i]];
+         curr[j] = max(take,noTake);
+       }
+       prev=curr;
+   }
+   cout<<prev[w]<<endl;
 }
