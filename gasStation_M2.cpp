1ad185875105fce9dbc9dfe39903f9bc1482fd37
HarshKumar Jha <jhaharsh878@gmail.com>
2023-01-07 21:36:06 +0530

    added gasStation_M2

--- gasStation_M2.cpp
+++ gasStation_M2.cpp
@@ -1,36 +1,23 @@
-//this is a solution with time complexity of O(n*n) and space complexity of O(1);
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int canCompleteCircuit(vector<int> &gas, vector<int> &cost){
-   int totalCoast = 0,totalCoastCpy, gasOverloaded = 0;
+   int gasConsumed = 0, gasAvailable = 0, startindex = 0, gasRequired = 0, gasAv = 0, gasReq = 0;
    for (int i = 0; i < gas.size(); i++){
-      totalCoast += cost[i];
-   }
-   int i = 0;
-   int j = 0;
-   totalCoastCpy=totalCoast;
-   while (totalCoast != 0 && j < gas.size()){
-      gasOverloaded += gas[i];
-      if(gasOverloaded >= cost[i]){
-         totalCoast -= cost[i];
-         gasOverloaded -= cost[i];
-         if (i >= gas.size() - 1){
-            i = (i + 1) % gas.size();
-         }
-         else{
-            i++;
-         }
-      }
-      else{
-         gasOverloaded = 0;
-         totalCoast = totalCoastCpy;
-         j = j + 1;
-         i = j;
+      gasAv += gas[i];
+      gasReq += cost[i];
+      gasConsumed += gas[i] - cost[i];
+      if (gasConsumed < 0){
+         startindex = i + 1;
+         gasConsumed = 0;
+         gasAvailable += gasAv;
+         gasAv = 0;
+         gasRequired += gasReq;
+         gasReq = 0;
       }
    }
-   return totalCoast==0?j:-1;
+
+   return gasRequired <= (gasAvailable + gasConsumed) ? startindex : -1;
 }
 
 int main(){
