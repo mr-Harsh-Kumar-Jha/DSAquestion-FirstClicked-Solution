18bc6d77b65b02944aab070b28d1d0bf5b479ba1
HarshKumar Jha <jhaharsh878@gmail.com>
2023-01-10 01:32:50 +0530

    modofied max_points_on_line_m2

--- max_points_on_line_m2.cpp
+++ max_points_on_line_m2.cpp
@@ -14,7 +14,7 @@ int maxPoints(vector<vector<int>> &points){
          count = 2;
          int dy = points[j][1] - points[i][1];
          int dx = points[j][0] - points[i][0];
-         for (int k = 0; k < n; k++){
+         for (int k = j+1; k < n; k++){
             if (k != i && k != j){
                int dy1 = points[k][1]-points[i][1];
                int dx1 = points[k][0]-points[i][0];
