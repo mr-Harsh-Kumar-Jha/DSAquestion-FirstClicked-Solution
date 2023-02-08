17283a9f19f4e5033fadb46d429ae2d21ae2d294
HarshKumar Jha <jhaharsh878@gmail.com>
2023-02-08 13:35:13 +0530

    added larges_subarrayCount_sum

--- Sliding-window-Algorithm/larges_subarrayCount_sum.cpp
+++ Sliding-window-Algorithm/larges_subarrayCount_sum.cpp
@@ -1,10 +1,10 @@
-//this solution will not work for array containing negative elements ,as u we can make an observation if the array contains neative elements then all the cases where the sum will be greater than target and the negative elemnts is present after that the due to third  base condition we can conclude that elemnts will start from (i+1)th index and which will result into missing of the cases . eg vec{3,1,1,1,1,0,0,-1,-2,1,2,3,5} here ans should be 10 but ans is 9 only.
+//this solution will not work for array containing negative elements ,as we can make an observation if the array contains neative elements then all the cases where the sum will be greater than target and the negative elements is present after that the due to third  base condition we can conclude that elemnts will start from (i+1)th index and which will result into missing of the cases . eg vec{3,1,1,1,1,0,0,-1,-2,1,2,3,5} here ans should be 10 but ans is 3 only.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-   vector<int>vec{3,1,1,1,1,0,0,1,2,3,5};
+   vector<int>vec{3,1,1,1,1,0,0,-1,-2,1,2,1,3,5};
    int i=0 , n=vec.size(),j=0,k=5,sum=0;
    int max=INT_MIN;
    while(j<n){
@@ -12,19 +12,20 @@ int main(){
          sum+=vec[j];
          j++;
       }
-      if(sum==k){
+     else if(sum==k){
          sum+=vec[j];
          if((j-i)>max){
             max = (j-i);
          }
          j++;
       }
-      if(sum>k){
-         // cout<<sum<<" ";
+      else if(sum>k){
+         sum+=vec[j];
          while(sum>k){
             sum-=vec[i];
             i++;
          }
+         j+=1;
       }
    }
    cout<<max<<endl;
