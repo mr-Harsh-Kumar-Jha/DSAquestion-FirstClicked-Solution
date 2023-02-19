774026788d85e981ce78a9f92bb190009cb24d2c
HarshKumar Jha <jhaharsh878@gmail.com>
2023-02-19 14:51:13 +0530

    modified longest_common_subsiquence

--- DP (Dynammic Programming)/longest_common_subsiquence.cpp
+++ DP (Dynammic Programming)/longest_common_subsiquence.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int longestCommonSub(string &s1,string &s2,int n,int m,vector<vector<int>>&dp){
    if(n<0 || m<0) return 0;
+   if(dp[n][m]!=-1) return dp[n][m];
    if(s1[n]==s2[m]) return dp[n][m] = 1+longestCommonSub(s1,s2,n-1,m-1,dp);
    return dp[n][m]=0+max(longestCommonSub(s1,s2,n-1,m,dp),longestCommonSub(s1,s2,n,m-1,dp));
 }
@@ -14,6 +15,6 @@ int main(){
    string s2 = "edgf";
    int n = s1.length();
    int m = s2.length();
-   vector<vector<int>>dp(n,vector<int>(m,0));
+   vector<vector<int>>dp(n,vector<int>(m,-1));
    cout<<longestCommonSub(s1,s2,n-1,m-1,dp)<<endl;
 }
