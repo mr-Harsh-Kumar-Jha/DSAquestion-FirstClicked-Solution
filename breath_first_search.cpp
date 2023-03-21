8b281ad0cc15da282d6a950e7629ad3dbe64263b
HarshKumar Jha <jhaharsh878@gmail.com>
2023-03-21 23:29:26 +0530

    added breath_first_search

---
+++ breath_first_search.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{  // declared in order to construct tree using linkedlist .
+  int data;
+  node* right=NULL;
+  node* left=NULL;
+};
+
+void bfs(node* head){
+   queue<node*>st;
+   st.push(head);
+   while(!st.empty()){
+      head = st.front();
+      cout<<head->data<<" ";
+      st.pop();
+      if(head->left!=NULL) st.push(head->left);
+      if(head->right!=NULL) st.push(head->right);
+   }
+}
+
+int main(){
+   struct node* Head = new struct node();
+   node* second = new struct node();
+   node* third = new struct node();
+   node* forth = new struct node();
+   node * fifth = new struct node();
+   node* sixth = new struct node();
+   node* seventh = new struct node();
+   node* eighth = new struct node();
+
+   Head->data=4;
+   Head->left=second;
+   Head->right=third;
+   second->data=5;
+   second->left=forth;
+   third->data=6;
+   third->right=fifth;
+   forth->data=7;
+   forth->left=seventh;
+   forth->right=sixth;
+   fifth->data=8;
+   fifth->right=eighth;
+   sixth->data=9;
+   seventh->data=1;
+   eighth->data=0;
+
+   bfs(Head);
+}
