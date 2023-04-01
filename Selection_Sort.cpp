f5ca8f951be697f0779466546648898ba4e72128
HarshKumar Jha <jhaharsh878@gmail.com>
2023-04-01 23:56:39 +0530

    modified Selection_Sort

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,33 +1,32 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-template<class t>void swap(t *a , t *b){
-    t temp = *a;
+void swap(int *a ,int *b){
+    int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-template< class t > void selectionSort(t arr[],int n){
-    for(int i=0;i<n;i++){
+void selectionSort(vector<int> &arr,int n){
+    for(int i=0;i<n-1;i++){
+      int temp = i;
         for(int j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                swap(&arr[i],&arr[j]);
+            if(arr[j]<arr[temp]){
+               temp = j;
             }
         }
+        if(temp!=i){
+            swap(&arr[i],&arr[temp]);
+        }
     }
+   for(int i=0;i<n;i++){
+      cout<<arr[i]<<" ";
+   }
+   cout<<endl;
 }
 
-int main()
-{
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-   selectionSort(arr,n);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    return 0;
+int main(){
+  vector<int>vec{2,1,4,3};
+  int n = vec.size();
+  selectionSort(vec,n);
 }
