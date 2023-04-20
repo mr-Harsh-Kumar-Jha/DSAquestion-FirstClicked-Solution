b1ce3ad22255355925d7c92cd4b5d3ba0e318438
HarshKumar Jha <jhaharsh878@gmail.com>
2023-04-20 23:37:17 +0530

    added Diameter_of_binary_tree

---
+++ Diameter_of_binary_tree.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{  // declared in order to construct tree using linkedlist .
+  int data;
+  node* right=NULL;
+  node* left=NULL;
+};
+
+int maxi = INT_MIN;
+
+int diameterOfBinaryTree(node* head){
+   if(head==NULL) return 0;
+   int lh = diameterOfBinaryTree(head->left);
+   int rh = diameterOfBinaryTree(head->right);
+   maxi=max(maxi,(lh+rh));
+   return 1+max(lh,rh);
+}
+
+int main(){
+   struct node* Head = new struct node();
+   node* second = new struct node();
+   node* third = new struct node();
+   node* forth = new struct node();
+   node * fifth = new struct node();
+   node* sixth = new struct node();
+   node* seventh = new struct node();
+   node* eighth = new struct node();
+
+   Head->data=4;
+   Head->left=second;
+   Head->right=third;
+   second->data=5;
+   second->left=forth;
+   third->data=6;
+   third->right=fifth;
+   forth->data=7;
+   forth->left=seventh;
+   forth->right=sixth;
+   fifth->data=8;
+   fifth->right=eighth;
+   sixth->data=9;
+   seventh->data=1;
+   eighth->data=0;
+   diameterOfBinaryTree(Head);
+   cout<<maxi<<endl;
+}
