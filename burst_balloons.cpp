b21cf965bff4ce35b5ffde8972b3aff9cff87a2b
Harsh Jha <jhaharsh878@gmail.com>
2023-07-21 22:03:57 +0530

    modified burst_balloons

--- burst_balloons.cpp
+++ burst_balloons.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int burstBalloons(vector<int>&vec,int i,int j){
+int burstBalloons(vector<int>&vec,int i,int j, vector<vector<int>>&dp){
    if(i>j) return 0;
+   if(dp[i][j]!=-1) return dp[i][j];
    int mini = INT_MIN;
    for(int ind = i;ind<=j;ind++){
-      int cost = (vec[i-1]*vec[ind]*vec[j+1]) + burstBalloons(vec,i,ind-1) + burstBalloons(vec, ind+1,j);
+      int cost = (vec[i-1]*vec[ind]*vec[j+1]) + burstBalloons(vec,i,ind-1,dp) + burstBalloons(vec, ind+1,j,dp);
       mini = max(mini,cost);
    }
-   return mini;
+   return dp[i][j]=mini;
 }
 
 int main(){
@@ -16,5 +17,6 @@ int main(){
    int n = vec.size();
    vec.insert(vec.begin(), 1);
    vec.push_back(1);
-   cout<<burstBalloons(vec,1,n)<<endl;
+   vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+   cout<<burstBalloons(vec,1,n,dp)<<endl;
 }
