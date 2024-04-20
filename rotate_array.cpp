e604d3aa069dd6fe48ee4fa3bd2ad5777a421508
Harsh Jha <jhaharsh878@gmail.com>
2024-04-20 23:48:50 +0530

    added rotate_array

---
+++ rotate_array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+
+        reverse(nums.begin(), nums.begin() + k);
+
+        reverse(nums.begin() + k, nums.end());
+    }
+};
