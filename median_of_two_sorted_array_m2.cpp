9b12814af0d4e79329f987d4f01412c841aba7ad
Harsh Jha <jhaharsh878@gmail.com>
2024-09-22 22:47:26 +0530

    added median_of_two_sorted_array_m2.cpp

---
+++ median_of_two_sorted_array_m2.cpp
@@ -0,0 +1,40 @@
+// here we have used two pointer approach for solving the question and you will be amazed with the solution 
+// as both the array are sorted we can use two pointers. If u will see it as a merging process u will realize that we are doing the same what we don in merging process the thing is that we are just storing the required elements with us. 
+// time complexity:- O(m+n)
+// space complexity:- O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j = 0, m1 = 0, m2 = 0;
+
+        // Find median.
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j]) {
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
+                }
+            } else if (i < n) {
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
+            }
+        }
+
+        // Check if the sum of n and m is odd.
+        if ((n + m) % 2 == 1) {
+            return static_cast<double>(m1);
+        } else {
+            double ans = static_cast<double>(m1) + static_cast<double>(m2);
+            return ans / 2.0;
+        }
+    }
+};
